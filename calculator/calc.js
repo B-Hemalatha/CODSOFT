ced7a91c6d2dde3dcb0073ed8d8d187b1e95f41a
B-Hemalatha <hemalathaababu2003@gmail.com>
2024-06-17 18:21:09 +0530

    add js

---
+++ calculator/calc.js
@@ -0,0 +1,17 @@
+function solve(val){
+    var a=document.getElementById('ans');
+    a.value+=val;
+}
+function  result(){
+    var n=document.getElementById('ans').value;
+    var nn=eval(n);
+    document.getElementById('ans').value=nn;
+}
+function clr(){
+    var w=document.getElementById('ans');
+    w.value='';
+}
+function del(){
+    var e=document.getElementById('ans');
+    e.value=e.value.slice(0,-1);
+}
